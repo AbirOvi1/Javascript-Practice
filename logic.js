4651be3e5d7898e6572bfddb7075e444e2e5f0df
AbirOvi1 <abiroviux@gmail.com>
2025-12-26 04:24:19 +0600

    another change!

--- logic.js
+++ logic.js
@@ -32,4 +32,39 @@ student ['fullName'] = 'Abir Ovi';
 console.log (student ['fullName']); */
 
 
-alert ('Hello, Abir!');
+// alert ('Hello, Abir!');
+
+// let a = 11;
+// console.log(a);
+
+// let a = 13;
+// console.log(a--);
+
+// console.log(11>9);
+// console.log(11<9); 
+
+// let myAge = 33;
+// let myStatus = (myAge > 18) ? 'you can vote!' : 'you can\'t vote!';
+// console.log (myStatus);
+
+// let myAge = 33;
+// let myStatus = (myAge < 18) ? 'you can vote!' : 'you can\'t vote!';
+// console.log (myStatus);
+
+// let age = 21;
+
+// if (age >= 18) {console.log ('You can visti!');}
+// else {console.log ('Sorry! You can\'t visit!');}
+
+// let age = 21;
+
+// if (age <= 18) {console.log ('You can visti!');}
+// else {console.log ('Sorry! You can\'t visit!');}
+
+
+// for (let x = 1; x<=11; x++)
+// {console.log ('AbirOvi!');}
+
+// for (let myName = 1; myName<=11; myName++)
+// {console.log ('My name is AbirOvi!');}
+
