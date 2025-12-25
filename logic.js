40bcaf91e6f439d90a77fe146790ae2c5f592273
AbirOvi1 <abiroviux@gmail.com>
2025-12-25 20:29:10 +0600

    added some changes.

---
+++ logic.js
@@ -0,0 +1,35 @@
+/* console.log ('Hello, Abir!');
+alert('Welcome, AbirOvi! Let\'s start learning Javascript!');
+
+a = 45949549;
+b =979587438;
+
+console.log (a*b);
+
+
+myName = 'AbirOvi';
+console.log (myName);
+
+{
+    let myName = 'Alu';
+    console.log (myName);
+}
+
+{
+    let yourName ='Lalu';
+    console.log (yourName);
+}
+
+
+const student = {
+    fullName : 'Abir Dutta',
+    age : 32,
+    cgpa : 5.00,
+    isPass : true,
+};
+
+student ['fullName'] = 'Abir Ovi';
+console.log (student ['fullName']); */
+
+
+alert ('Hello, Abir!');
